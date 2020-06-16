933586593412ba0be84329e591f56ba4a76cca5d
Aria Keshmiri <ariakesh@yahoo.com>
2020-06-16 10:58:28 -0700

    added couple changes to return statement teting stackery

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,6 +1,9 @@
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
-
-  return {};
+console.log('look mamma its working')
+  return {
+    statusCode:200,
+    body:'im testing deploying a stackery environment'
+  };
 };
